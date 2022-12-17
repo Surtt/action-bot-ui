5f199874e825bfa4a13a2596b79bde2f977525cb
Alexander Sergeev <aleksanteri.sergeev@gmail.com>
2022-12-17 20:56:23 +0200

    init project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'antd/dist/reset.css';
+import './App.css';
+import {Button} from "antd";
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary">Button</Button>
+    </div>
+  );
+}
+
+export default App;
